908897cabbbb79183d8631e4192fb34651dfd245
Master.G <master-g@users.noreply.github.com>
2025-02-28 20:42:03 +0800

    feat: restore to original tutorial

--- code/integration-and-debugging/wgpu_in_web/src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub mod wgpu_app;
-
-#[cfg(target_arch = "wasm32")]
-mod web_ffi;
-#[cfg(target_arch = "wasm32")]
-pub use web_ffi::*;
-
-#[cfg(not(target_arch = "wasm32"))]
-pub mod native_wrapper;
